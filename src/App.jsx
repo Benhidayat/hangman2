b14026fb74cf97f96576e97482f827136edc6b95
benhidayat <98013492+Benhidayat@users.noreply.github.com>
2025-01-23 12:25:24 +0700

    keyboard button change color to green or red
    create a state to save guessed letter
    change the color of a button if it already guessed
    and if it includes in currentWord

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import './App.css';
 import { Header, Status, Languages, Word, Keyboard } from './components';
 import { useState } from 'react';
+import clsx from 'clsx';
 
 const App = () => {
   // state value
   const [currentWord, setCurrentWord] = useState('react');
+  const [guessedLetters, setGuessedLetter] = useState([]);
 
   // static value
   const alphabets = 'abcdefghijklmnopqrstuvwxyz';
 
+  // save the guessed letters into a state
+  const addGuessLetter = (letter) => {
+    setGuessedLetter(prevGuessed => (
+      prevGuessed.includes(letter) 
+        ? prevGuessed
+        : [...prevGuessed, letter]          
+    ))
+  }
+
   // letter
   const letterElements = [...currentWord].map((letter, index) => {
+
     return (
       <span key={index}
             className='letter'>
@@ -21,8 +33,18 @@ const App = () => {
 
   // keyboard
   const keyboardElements = [...alphabets].map(letter => {
+    const isInside = [...currentWord].includes(letter) && guessedLetters.includes(letter);
+    const isNotInside = guessedLetters.includes(letter) && ![...currentWord].includes(letter);
+    
+    const className = clsx({
+      right: isInside,
+      wrong: isNotInside
+    })
     return (
-      <button key={letter}>
+      <button key={letter}
+              type='button'
+              className={className}
+              onClick={() => addGuessLetter(letter)}>
         {letter.toLocaleUpperCase()}
       </button>
     )
