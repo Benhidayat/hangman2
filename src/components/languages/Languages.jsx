5366042e6b698144a32346b2bea23317e2a1f0ff
benhidayat <98013492+Benhidayat@users.noreply.github.com>
2025-01-24 12:08:42 +0700

    change the languages appearance into skull every time player makes the incorrect guess

--- src/components/languages/Languages.jsx
+++ src/components/languages/Languages.jsx
@@ -1,16 +1,21 @@
 import './Languages.css';
 import { languages } from '../../data/languages';
+import clsx from 'clsx';
 
-const Languages = () => {
-    const langElements = languages.map(lang => {
+const Languages = ({guessCount}) => {
+    const langElements = languages.map((lang, index) => {
+        const isLangLost = index < guessCount;
+        const lostLang = clsx(isLangLost ? 'lost' : null)  
+
         const styles = {
             backgroundColor: lang.backgroundColor,
             color: lang.color
         }
-        return <span key={lang.name}
-                     style={styles}>
+        return <div key={lang.name}
+                     style={styles}
+                     className={lostLang}>
                     {lang.name}
-                </span>
+                </div>
     })
   return (
     <section className='languages'>
